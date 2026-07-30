45da8cb98de519baf60aee898f4717f13e6c9dda
Kartik Chilkoti <163172119+chilkotiKartik@users.noreply.github.com>
2026-07-30 10:41:09 +0530

    Time: 0 ms (100%), Space: 19.5 MB (15.8%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -0,0 +1,33 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        INT_MAX = 2**31 - 1
+        INT_MIN = -2**31
+
+        i = 0
+        n = len(s)
+
+        # Skip leading spaces
+        while i < n and s[i] == " ":
+            i += 1
+
+        # Check sign
+        sign = 1
+        if i < n and (s[i] == "+" or s[i] == "-"):
+            if s[i] == "-":
+                sign = -1
+            i += 1
+
+        num = 0
+
+        # Read digits
+        while i < n and s[i].isdigit():
+            digit = int(s[i])
+
+            # Overflow check
+            if num > (INT_MAX - digit) // 10:
+                return INT_MAX if sign == 1 else INT_MIN
+
+            num = num * 10 + digit
+            i += 1
+
+        return sign * num
